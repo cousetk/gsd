f6ac02ce2db601e8dfcbad5d6ea8322ee6483d24
Karim COUSET <karim.couset@ext.europarl.europa.eu>
2024-01-12 21:27:09 +0100

    2nd stage

--- container/cmd.ps1
+++ container/cmd.ps1
@@ -3,3 +3,25 @@ docker --version
 docker image build -t cousetk/gsd:ctr2023 .
 
 docker image ls
+
+docker image push cousetk/gsd:ctr2023
+
+# docker buildx build `
+#     --platform linux/arm64/v8,linux/amd64 `
+#     --push `
+#     --tag cousetk/gsd:ctr2023 `
+#     .
+
+docker image rm cousetk/gsd:ctr2023
+
+docker container run -d --name web -p 8000:8080 cousetk/gsd:ctr2023
+
+docker container ls
+docker container ls -a
+
+docker container stop web
+docker container start web
+docker container rm web
+
+docker container run -it --name test alpine sh
+docker container rm test -f
